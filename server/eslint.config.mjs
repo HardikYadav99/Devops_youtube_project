d1892263e8305823de6f6f9be501702126d2c90a
HardikYadav99 <hardikyadav1567@gmail.com>
2026-03-30 03:28:07 +0530

    removed conflicting if from CI files

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -1,9 +1,9 @@
 export default [
-  {
-    files: ['**/*.js'], // check all the .js file in the server directory
-    rules: {
-      semi: 'error', //force semicolons
-      'no-unused-vars': 'warn', // warns is variable is unused
+    { 
+        files: ["**/*.js"], // check all the .js file in the server directory 
+        rules: {
+            semi: "error", //force semicolons 
+            "no-unused-vars": "warn", // warns is variable is unused
+        },
     },
-  },
 ];
