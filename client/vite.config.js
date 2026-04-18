53c4c22ccaa87c2c4ff036c6fdfdd10dc8292206
HardikYadav99 <hardikyadav1567@gmail.com>
2026-04-18 13:49:28 +0530

    Added k8s file, this project is complete now

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,4 +5,16 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  // This section is for 'npm run dev'
+  server: {
+    allowedHosts: ["merndemo.local"],
+    host: true,
+    port: 5173,
+  },
+  // This section is for 'npm run preview' (which your Dockerfile uses)
+  preview: {
+    allowedHosts: ["merndemo.local"],
+    host: true,
+    port: 5173,
+  },
 });
